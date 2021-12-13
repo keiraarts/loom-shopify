2e80a450d093bc01b0266250f37292b30591a764
keiraarts <keiraarts@gmail.com>
2021-12-12 19:40:31 -0500

    add new section for Shopify product sections

--- i18n.js
+++ i18n.js
@@ -14,20 +14,22 @@ const resources = {
   },
 };
 
-i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    resources,
-    fallbackLng: "en",
-    interpolation: {
-      escapeValue: false, // react already safes from xss
-    },
-  });
+const translations = () => {
+  i18n
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(LanguageDetector)
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+      resources,
+      fallbackLng: "en",
+      interpolation: {
+        escapeValue: false, // react already safes from xss
+      },
+    });
+};
 
-export default i18n;
+export default translations;
